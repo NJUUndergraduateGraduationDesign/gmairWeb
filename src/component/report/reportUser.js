89cbb1cfde90fad74b62b50277a60760c0118b1a
chenguangming <980231201@qq.com>
2020-03-09 12:08:05 +0800

    finish: report.

--- src/component/report/reportUser.js
+++ src/component/report/reportUser.js
@@ -6,6 +6,9 @@ import Footer from "../header/footer";
 import {reportService} from "../../service/report.service";
 import "./reportUser.css"
 
+const envLv = ["优", "较优" , "一般", "较差", "差"];
+const envSuggest = ["太棒了，请保持！", "加油，争取更好！", "还有提升空间哦！", "请多多使用本设备改善室内环境！", "请多多使用本设备改善室内环境！"];
+
 class ReportUser extends Component {
     constructor(props) {
         super(props);
@@ -26,8 +29,47 @@ class ReportUser extends Component {
             pm25Average : 0.0,
             defeatUserPercent : 0.0,
 
-            categoryEnvironment : 0,
+            categoryEnvironment : "",
+            environmentSuggest : "",
+
+            part1 : {
+                first : true,
+                text1 : "statisticText1WithAnim",
+                text2 : "statisticText2WithAnim",
+                text3 : "statisticText3WithAnim",
+                text4 : "statisticText4WithAnim",
+            },
+
+            part2 : {
+                first : true,
+                text1 : "statisticText1",
+                text2 : "statisticText2",
+                text3 : "statisticText3",
+            },
+
+            part3 : {
+                first : true,
+                text1 : "statisticText5",
+                text2 : "statisticText6",
+                text3 : "statisticText7",
+            },
+
+            part4 : {
+                first : true,
+                text1 : "statisticText8",
+                text2 : "statisticText9",
+                text3 : "statisticText10",
+            },
+
+            part5 : {
+                first : true,
+                text1 : "statisticText1",
+                text2 : "statisticText2",
+                text3 : "statisticText3",
+            },
         };
+
+        this.onChange = this.onChange.bind(this);
     }
 
     componentDidMount() {
@@ -47,15 +89,60 @@ class ReportUser extends Component {
                 pm25Average: response.data.pm25Average,
                 defeatUserPercent: response.data.defeatUserPercent,
 
-                categoryEnvironment: response.data.categoryEnvironment
+                categoryEnvironment: envLv[response.data.categoryEnvironment],
+                environmentSuggest: envSuggest[response.data.categoryEnvironment],
             });
         });
     }
 
+    onChange(current) {
+        if (current === 1 && this.state.part2.first) {
+            this.setState({
+                part2 : {
+                    first : false,
+                    text1 : "statisticText1WithAnim",
+                    text2 : "statisticText2WithAnim",
+                    text3 : "statisticText3WithAnim",
+                },
+            })
+        }
+        else if (current === 2 && this.state.part3.first) {
+            this.setState({
+                part3 : {
+                    first : false,
+                    text1 : "statisticText5WithAnim",
+                    text2 : "statisticText6WithAnim",
+                    text3 : "statisticText7WithAnim",
+                },
+            })
+        }
+        else if (current === 3 && this.state.part4.first) {
+            this.setState({
+                part4 : {
+                    first : false,
+                    text1 : "statisticText8WithAnim",
+                    text2 : "statisticText9WithAnim",
+                    text3 : "statisticText10WithAnim",
+                },
+            })
+        }
+        else if (current === 4 && this.state.part5.first) {
+            this.setState({
+                part5 : {
+                    first : false,
+                    text1 : "statisticText1WithAnim",
+                    text2 : "statisticText2WithAnim",
+                    text3 : "statisticText3WithAnim",
+                },
+            })
+        }
+    }
+
     render() {
         const carouselSettings = {
-            autoplay: false,
-            autoplaySpeed: 5000,
+            autoplay: true,
+            autoplaySpeed: 6000,
+            afterChange: this.onChange
         };
 
         return <div>
@@ -69,33 +156,46 @@ class ReportUser extends Component {
                 <div style={{backgroundColor: `white`, paddingTop: `0px`}}>
                     <Carousel {...carouselSettings} className={'carouselCss'}>
                         <div>
-                            <p className={"statisticText1"}>在最近30天中......</p>
-                            <p className={"statisticText2"}>您的设备共有 {this.state.openDaysCount} 天处于运行状态；</p>
-                            <p className={"statisticText3"}>{this.state.mostOpenDay}，您的设备运行了最长的时间，</p>
-                            <p className={"statisticText4"}>最长时间为 {this.state.mostOpenDayHoursCount} 分钟！</p>
+                            <p className={this.state.part1.text1}>在最近30天中......</p>
+                            <p className={this.state.part1.text2}>您的设备共有 <span className={'special-font'}>
+                                {this.state.openDaysCount}</span> 天处于运行状态；</p>
+                            <p className={this.state.part1.text3}><span className={'special-font'}>
+                                {this.state.mostOpenDay}</span>，您的设备运行了最长的时间，</p>
+                            <p className={this.state.part1.text4}>最长时间为
+                                <span className={'special-font'}>{this.state.mostOpenDayHoursCount}</span> 分钟！</p>
                             <img src={require("../../../public/carouselImg/1.jpg")} alt={'#'} className={"bgImgCss"}/>
                         </div>
                         <div>
-                            <p className={"statisticText1"}>在最近30天中......</p>
-                            <p className={"statisticText2"}>您经常在 {this.state.mostOpenHourGTE} 时到 {this.state.mostOpenHourLTE} 时之间使用设备；</p>
-                            <p className={"statisticText3"}>在这个时间段内，您平均使用了 {this.state.mostOpenHourMinutesCount} 分钟！</p>
+                            <p className={this.state.part2.text1}>在最近30天中......</p>
+                            <p className={this.state.part2.text2}>您经常在 <span className={'special-font'}>
+                                {this.state.mostOpenHourGTE}</span> 时到 <span className={'special-font'}>
+                                {this.state.mostOpenHourLTE}</span> 时之间使用设备；</p>
+                            <p className={this.state.part2.text3}>在这个时间段内，您平均使用了 <span className={'special-font'}>
+                                {this.state.mostOpenHourMinutesCount}</span> 分钟！</p>
                             <img src={require("../../../public/carouselImg/2.jpg")} alt={'#'} className={"bgImgCss"}/>
                         </div>
                         <div>
-                            <p className={"statisticText5"}>在最近30天中......</p>
-                            <p className={"statisticText6"}>您最常使用的设备模式是：{this.state.mostUseMode}；</p>
-                            <p className={"statisticText7"}>该模式您使用了 {this.state.mostUseModeHoursCount} 小时！</p>
+                            <p className={this.state.part3.text1}>在最近30天中......</p>
+                            <p className={this.state.part3.text2}>您最常使用的设备模式是：<span className={'special-font'}>
+                                {this.state.mostUseMode}</span>；</p>
+                            <p className={this.state.part3.text3}>该模式您使用了 <span className={'special-font'}>
+                                {this.state.mostUseModeHoursCount}</span> 小时！</p>
                             <img src={require("../../../public/carouselImg/3.jpg")} alt={'#'} className={"bgImgCss"}/>
                         </div>
                         <div>
-                            <p className={"statisticText5"}>在最近30天中......</p>
-                            <p className={"statisticText6"}>您的室内平均PM2.5指数是；{this.state.pm25Average}；</p>
-                            <p className={"statisticText7"}>打败了全国 {this.state.defeatUserPercent} % 的用户！继续加油！</p>
+                            <p className={this.state.part4.text1}>在最近30天中......</p>
+                            <p className={this.state.part4.text2}>您的室内平均PM2.5指数是；<span className={'special-font'}>
+                                {this.state.pm25Average}</span>；</p>
+                            <p className={this.state.part4.text3}>打败了全国 <span className={'special-font'}>
+                                {this.state.defeatUserPercent}</span> % 的用户！继续加油！</p>
                             <img src={require("../../../public/carouselImg/4.jpg")} alt={'#'} className={"bgImgCss"}/>
                         </div>
                         <div>
-                            <p className={"statisticText1"}>在最近30天中......</p>
-                            <p className={"statisticText2"}>经分析，您的室内环境类别为：{this.state.categoryEnvironment}。</p>
+                            <p className={this.state.part5.text1}>在最近30天中......</p>
+                            <p className={this.state.part5.text2}>经分析，您的室内环境类别为：<span className={'special-font'}>
+                                {this.state.categoryEnvironment}</span>。</p>
+                            <p className={this.state.part5.text3}><span className={'special-font'}>
+                                {this.state.environmentSuggest}</span></p>
                             <img src={require("../../../public/carouselImg/5.jpg")} alt={'#'} className={"bgImgCss"}/>
                         </div>
                     </Carousel>
